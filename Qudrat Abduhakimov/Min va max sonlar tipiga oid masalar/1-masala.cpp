77243885a5601439ecfdafa8ebd364be1895ed0f
Valisher Botirov <valisherbotirov1218@gmail.com>
2022-05-17 15:34:31 +0500

    Qudrat Abduhakimov masalalar to'plami minumum va maxsimum sonlar bobidan 1-masala

--- Qudrat Abduhakimov/Min va max sonlar tipiga oid masalar/1-masala.cpp
+++ Qudrat Abduhakimov/Min va max sonlar tipiga oid masalar/1-masala.cpp
@@ -3,9 +3,10 @@ using namespace std;
 int main(){
 	int m;
 	cin >> m;
-	int max = 0;
 	int a ;
-	for(int i = 0 ; i < m ; i++){
+	cin>>a;
+	int max = a;
+	for(int i = 1 ; i < m ; i++){
 		cin>>a;
 		if(a > max){
 			max = a;
