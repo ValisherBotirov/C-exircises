6477dae92afbac834f0d8b0d9834a5907d1b545d
Valisher Botirov <valisherbotirov1218@gmail.com>
2022-05-18 23:32:18 +0500

    C++ yangi masalalar

---
+++ Dasturlash 2-smester/4.1-topshiriq.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<deque>
+#include <time.h>
+using namespace std;
+int main(){
+	srand(time(0));
+	deque <int> numbers;
+	int n;
+	cout<<"n= ";
+	cin>>n;
+	for(int i=1; i<=n; i++){
+		numbers.push_back(rand()%30);
+	}
+	
+	
+	int min = *numbers.begin();
+	deque<int> ::iterator k;
+	for(auto it = numbers.begin(); it!=numbers.end(); ++it){
+		cout<<*it<<" ";
+		if(min > *it){
+			min = *it;
+			k = it;
+		}
+	}
+	numbers.insert(k+1,0);
+	cout<<"\n\nMin = > ";
+	cout<<min;
+	cout<<"\n\n";
+	
+	for(auto it = numbers.begin(); it!=numbers.end(); ++it){
+		cout<<*it<<" ";
+	}
+	
+	
+
+	
+	return 0;
+}
